f4ae86605dd19eef650e2b939c96c8a345eebd49
alex-jones <abjhoops@gmail.com>
2023-02-15 13:00:10 -0700

    add route to index controller function and create and export said function

--- routes/dogs.js
+++ routes/dogs.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const dogsCtrl = require('../controllers/dogs.js')
 
 router.post('/', dogsCtrl.create)
+router.get('/', dogsCtrl.index)
 
 module.exports = router
